82d38372ec1d0599d290e721b1a9858be0557c02
Zoran <greedy0101@gmail.com>
2023-01-30 21:57:55 +0800

    add server function

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 import express from "express"
+import cors from "cors"
 const app = express();
 
 
-const user = [
+const users = [
   {
-    id : 0,
+    id : "0",
     name : "Jack",
     email : "Jack@gmail",
     password : "Jack123",
@@ -12,7 +13,7 @@ const user = [
     registerTime : new Date()
   },
   {
-    id : 1,
+    id : "1",
     name : "Tom",
     email : "Tom@gmail",
     password : "Tom123",
@@ -30,6 +31,8 @@ app.listen(3000, () => {
 
 //解析使用者需求
 app.use(express.json())
+// 跨網域需求(讓撰寫者可以在瀏覽器上測試全端專案)
+app.use(cors())
 
 app.get("", (req, res) =>{
   console.log("Connected")
@@ -38,7 +41,7 @@ app.get("", (req, res) =>{
 
 //使用者行為 : signin || 對應網路行為 : get || 結果 : 顯示成功或失敗
 app.post('/signIn', (req, res) => {
-  if (req.body.email === user[0].email && req.body.password === user[0].password){
+  if (req.body.email === users[0].email && req.body.password === users[0].password){
     res.json('Welcome your web.')
   }else{
     res.json("Fail to sign in")
@@ -46,12 +49,49 @@ app.post('/signIn', (req, res) => {
 })
 
 
-//使用者行為 : Register || 對應網路行為 : post || 結果 : 取得user資料
+//使用者行為 : Register || 對應網路行為 : post || 結果 : 傳輸user資料
 app.post("/", (req, res) => {
-  if (req.body.email !== user[0].email && req.body.password !== user[0].password){
+  if (req.body.email !== users[0].email && req.body.password !== users[0].password){
     res.send('Work Done.')
   }else{
     res.send("error")
   }
 })
+
+//使用者行為 : get users's own page || 對應網路行為 : get || 結果 : 返回user資料
+app.get("/profile/:id", (req, res) => {
+  const {id} = req.params
+  let found = false
+  users.forEach((user) => {
+      if (user.id === id){
+        found = true
+        user.uploadTime++
+        return res.json(user)
+      }
+    }
+  )
+  if (!found) {res.status(400).send("user not found")}
+})
+
+
+//使用者行為 : update 辨識圖片同時在database中更新辨識圖片的次數 || 對應網路行為 : put || 結果 : 更新使用者辨識圖片的次數
+app.put("/image", (req, res) => {
+  const {id} = req.params
+  let found = false
+  users.forEach((user) => {
+      if (user.id === id){
+        found = true
+        return res.json(user)
+      }
+    }
+  )
+  if (!found) {res.status(400).send("user not found")}
+})
+
+
+
+
+
+
+
 
