5bb67beec024500cb3463a355860a7ecece876a5
Zoran <greedy0101@gmail.com>
2023-02-14 21:00:44 +0800

    initial heroku commit

--- server/server.js
+++ server/server.js
@@ -6,22 +6,23 @@ const handleRegister = require("./controller/register.js")
 const handleSignin = require("./controller/signin.js")
 const handleProfile = require("./controller/profile.js")
 const handleImage = require("./controller/Image.js")
+const path = require('path');
 
 const app = express();
 
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'Facial-recognition-system'
+    connectionString: "//dzfbmjksdibueh:937c269a2c991c55d7a3ed79ca01667bff4b349f233422b0af4c297db50abbb8@ec2-3-230-122-20.compute-1.amazonaws.com:5432/d23e3k13npt2q1",
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
 //監聽使用者需求
 let PORT = process.env.PORT;
-if (PORT == null || PORT == "") {
+if (PORT == null || PORT === "") {
   PORT = 8000;
 }
 
@@ -29,10 +30,21 @@ app.listen(PORT, () => {
   console.log(`Server is working!!!!!!!!!`)
 })
 
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
 //解析使用者需求
 app.use(express.json());
 // 跨網域需求(讓撰寫者可以在瀏覽器上測試全端專案)
 app.use(cors());
+
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+ });
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //使用者行為 : signin || 對應網路行為 : get || 結果 : 顯示成功或失敗
 app.post('/signin', (req, res) => {handleSignin.handleSignin(req, res, bcrypt, db)});
